4e0d3d54dce1cef02ec898ad5fb10cd92202e2fb
Deskalisko <deskalisko@gmail.com>
2025-05-05 18:13:36 +0200

    LAB12
    Dodanie laba12

--- LABY/src/TASKS/Run.java
+++ LABY/src/TASKS/Run.java
@@ -1,6 +1,8 @@
 package TASKS;
 import TASKS.LAB07.zadania_domowe.MenuLab07;
 import TASKS.LAB08.ZadaniaDomowe.MenuLab08;
+import TASKS.LAB09.ZadaniaDomowe.MenuLab09;
+import TASKS.LAB12.ZADANIA.TaskLab12;
 
 
 import java.util.Scanner;
@@ -21,6 +23,9 @@ public class Run {
         System.out.println("7. Laboratorium 7");
         System.out.println("8. Laboratorium 8");
         System.out.println("9. Laboratorium 9");
+        System.out.println("10. Laboratorium 10");
+        System.out.println("11. Laboratorium 11");
+        System.out.println("12. Laboratorium 12");
         System.out.println("15. Zakończ program");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
@@ -52,6 +57,15 @@ public class Run {
             case 9:
                 MenuLab09RUN();
                 break;
+            case 10:
+                MenuLab10RUN();
+                break;
+            case 11:
+                MenuLab11RUN();
+                break;
+            case 12:
+                MenuLab12RUN();
+                break;
             case 15:
                 System.out.println("Program został zakonczony.");
                 System.exit(0);
@@ -61,9 +75,24 @@ public class Run {
         }
     }
 
+    private void MenuLab12RUN() {
+        TaskLab12.Zadanie1();
+
+    }
+
+    private void MenuLab11RUN() {
+        TASKS.LAB11.ZADANIA.MenuLab11 lab = new TASKS.LAB11.ZADANIA.MenuLab11();
+        lab.Menu();
+    }
+
+    private void MenuLab10RUN() {
+        TASKS.LAB10.Zadania.MenuLab10 lab = new TASKS.LAB10.Zadania.MenuLab10();
+        lab.Menu();
+    }
+
     private void MenuLab09RUN() {
-        TASKS.LAB09.ZadaniaDomowe.MenuLab09 lab09 = new TASKS.LAB09.ZadaniaDomowe.MenuLab09();
-        lab09.MENU();
+        TASKS.LAB09.ZadaniaDomowe.MenuLab09 lab = new TASKS.LAB09.ZadaniaDomowe.MenuLab09();
+        lab.MENU();
     }
 
     private void MenuLab08RUN() {
